9e68a84c7cbefe12b6a9acff57c3b22152474a4a
Emily Garcia <garcia.16.emily@gmail.com>
2020-06-01 20:43:59 -0500

    Realizamos distintos tipos de ejercicios con python

---
+++ advanced_calc.py
@@ -0,0 +1,30 @@
+num1 = float(input('Ingresa el primer numero: '))
+num2 = float(input('Ingresa el segundo numero: '))
+
+suma = num1+num2
+print('El resultado de la suma es',suma)
+
+resta = num1-num2
+print('El resultado de la resta es',resta)
+
+mul = num2 * num1
+print('El resultado de la multipliccion es',mul)
+
+div = num1 / num2
+print('El resultado de la division es',div)
+
+div_ex = num1 // num2
+print("El resultado de la division exacta es",div_ex)
+
+ele_cuad1 = num1 ** 2
+ele_cuad2 = num2 ** 2
+print('El resultado de elevar',num1,'al cuadrado es',ele_cuad1)
+print('El resultado de elevar',num2,'al cuadrado es',ele_cuad2)
+
+ele_cub1 = num1 ** 3
+ele_cub2 = num2 ** 3
+print('El resultado de elevar',num1,'al cubo es',ele_cub1)
+print('El resultado de elevar',num2,'al cubo es',ele_cub2)
+
+modulo = num1 % num2
+print('El resultado del modulo es ',modulo)
---
+++ basic_calc.py
@@ -0,0 +1,14 @@
+num1 = float(input('Ingresa el primer numero: '))
+num2 = float(input('Ingresa el segundo numero: '))
+
+suma = num1+num2
+print('El resultado de la suma es',suma)
+
+resta = num1-num2
+print('El resultado de la resta es',resta)
+
+mul = num2 * num1
+print('El resultado de la multipliccion es',mul)
+
+div = num1 / num2
+print('El resultado de la division es',div)
